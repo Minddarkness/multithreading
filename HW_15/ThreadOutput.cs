19cf1d5d3e80c2007a52691c743c57ec08257df0
Anastasiya Levina <miss.levinaayandex.ru>
2023-06-15 15:34:41 +0700

    HW_15 - add cycle and threads names

--- HW_15/ThreadOutput.cs
+++ HW_15/ThreadOutput.cs
@@ -2,23 +2,27 @@ namespace HW_15;
 
 public class ThreadOutput
 {
-    public ThreadOutput()
-    {
-        
-    }
+    private int _threadID = 0;
+    private List<Thread> _threads = new List<Thread>();
     
     public void WriteInThread(string text, int delay)
     {
         ThreadStart task = new ThreadStart(() => WriteWithDelay(text, delay));
         Thread thread = new Thread(task);
+        thread.Name = "Thread " + _threadID++;
+        _threads.Add(thread);
         Console.WriteLine("thread started");
         thread.Start();
     }
 
     private void WriteWithDelay(string text, int delay)
     {
-        var currentThread = Thread.CurrentThread;
-        Thread.Sleep(delay);
-        Console.WriteLine($"thread name: {currentThread.Name} text: {text}");
+        var currentThreadName = Thread.CurrentThread.Name;
+
+        while (true)
+        {
+            Thread.Sleep(delay);
+            Console.WriteLine($"thread name: {currentThreadName} text: {text}");
+        }
     }
 }
